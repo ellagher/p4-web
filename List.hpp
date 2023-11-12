c5892ee6798948981f347d36e224b9944b9d41bf
ellagher <ellagh@umich.edu>
2023-11-12 12:26:50 -0500

    Added list implementation in List.hpp and tests in List_tests.cpp

--- List.hpp
+++ List.hpp
@@ -16,61 +16,144 @@ class List {
   //OVERVIEW: a doubly-linked, double-ended list with Iterator interface
 public:
   List() {
-    
+    first = NULL;
+    last = NULL;
+  }
+
+  List(const List<T> &other) {
+    first = NULL;
+    last = NULL;
+    copy_all(other);
+  }
+
+  ~List() {
+    clear();
   }
 
   //EFFECTS:  returns true if the list is empty
   bool empty() const {
-    return false;
+    return size() == 0;
   }
 
   //EFFECTS: returns the number of elements in this List
   //HINT:    Traversing a list is really slow.  Instead, keep track of the size
   //         with a private member variable.  That's how std::list does it.
   int size() const {
-    return 0;
+    Node* iterator = first;
+    int size = 0;
+    while(iterator != NULL) {
+      iterator = iterator->next;
+      size++;
+    }
+    return size;
   }
 
   //REQUIRES: list is not empty
   //EFFECTS: Returns the first element in the list by reference
   T & front() {
-    return first;
+    assert(size() != 0);
+    return first->datum;
   }
 
   //REQUIRES: list is not empty
   //EFFECTS: Returns the last element in the list by reference
   T & back() {
-    return last;
+    assert(size() != 0);
+    return last->datum;
   }
 
   //EFFECTS:  inserts datum into the front of the list
   void push_front(const T &datum) {
-
+    // if list is empty, the front and back just become the element being added
+    if(size() == 0) {
+      Node* n = new Node();
+      n->next = NULL;
+      n->prev = NULL;
+      n->datum = datum;
+      first = n;
+      last = n;
+    }
+    else {
+      Node* n = new Node();
+      n->datum = datum;
+      n->next = first; // make the new node point at the previous head
+      n->prev = NULL; // no prev for the front
+      first->prev = n; // point current head at the new head
+      first = n; // set the head to the new node
+    }
   }
 
   //EFFECTS:  inserts datum into the back of the list
   void push_back(const T &datum) {
-
+    // if list is empty, the front and back just become the element being added
+    if(size() == 0) {
+      Node* n = new Node();
+      n->next = NULL;
+      n->prev = NULL;
+      n->datum = datum;
+      first = n;
+      last = n;
+    }
+    else {
+      Node* n = new Node();
+      n->datum = datum;
+      n->prev = last; // make new node's prev point to current tail
+      n->next = NULL; // make new node's prev point to NULL (end)
+      last->next = n; // make the tail's next point at this node
+      last = n; // make this node the new tail
+    }
   }
 
   //REQUIRES: list is not empty
   //MODIFIES: may invalidate list iterators
   //EFFECTS:  removes the item at the front of the list
   void pop_front() {
-
+    assert(size() != 0);
+    // handle special case of only one element in the list
+    if(size() == 1) {
+      delete(first);
+      first = NULL;
+      last = NULL;
+    }
+    else {
+      Node* temp = first;
+      first = first->next;
+      first->prev = NULL;
+      delete(temp);
+    }
   }
 
   //REQUIRES: list is not empty
   //MODIFIES: may invalidate list iterators
   //EFFECTS:  removes the item at the back of the list
   void pop_back() {
-
+    assert(size() != 0);
+    // handle special case of only one element in the list
+    if(size() == 1) {
+      delete(first);
+      first = NULL;
+      last = NULL;
+    }
+    else {
+      Node* temp = last;
+      last->prev->next = NULL; // make the new last point at null
+      last = last->prev; // update last to the prev
+      delete(temp);
+    }
   }
 
   //MODIFIES: may invalidate list iterators
   //EFFECTS:  removes all items from the list
   void clear() {
-
+    Node* cur = first;
+    Node* prev = NULL;
+    while(cur != NULL) {
+      prev = cur;
+      cur = cur->next;
+      delete(prev);
+    }
+    first = NULL;
+    last = NULL;
   }
 
   // You should add in a default constructor, destructor, copy constructor,
@@ -89,13 +172,20 @@ private:
   //REQUIRES: list is empty
   //EFFECTS:  copies all nodes from other to this
   void copy_all(const List<T> &other) {
+    assert(first == NULL);
+    Node* other_n = other.first;
+    while(other_n != NULL) {
+      push_back(other_n->datum);
+      other_n = other_n->next;
+    }
 
   }
 
   Node *first;   // points to first Node in list, or nullptr if list is empty
   Node *last;    // points to last Node in list, or nullptr if list is empty
+  
 
-public:
+public:  
   ////////////////////////////////////////
   class Iterator {
     //OVERVIEW: Iterator interface to List
@@ -117,14 +207,37 @@ public:
       return *this;
     }
 
+    bool operator==(const Iterator& rhs) const {
+      return node_ptr == rhs.node_ptr;
+    }
+
+    bool operator!=(const Iterator& rhs) const {
+      if(rhs == NULL) return false; // if rhs is null they are not equal
+      return node_ptr != rhs.node_ptr;
+    }
+
+    Iterator& operator++() {
+      assert(node_ptr);
+      node_ptr = node_ptr->next;
+      return *this;
+    }
+
+    T& operator*() {
+      return node_ptr->datum;
+    }
+
   private:
     Node *node_ptr; //current Iterator position is a List node
     // add any additional necessary member variables here
 
     // add any friend declarations here
+    friend class Node;
+    friend class List;
 
     // construct an Iterator at a specific position
-    Iterator(Node *p);
+    Iterator(Node *p) {
+      node_ptr = p;
+    }
 
   };//List::Iterator
   ////////////////////////////////////////
@@ -135,16 +248,47 @@ public:
   }
 
   // return an Iterator pointing to "past the end"
-  Iterator end() const;
+  Iterator end() const {
+    return Iterator(last->next);
+  }
 
   //REQUIRES: i is a valid, dereferenceable iterator associated with this list
   //MODIFIES: may invalidate other list iterators
   //EFFECTS: Removes a single element from the list container
-  void erase(Iterator i);
+  void erase(Iterator i) {
+    // case 1: remove from front
+    if(i.node_ptr == first) {
+      pop_front();
+    }
+    // case 2: remove from back
+    else if(i.node_ptr == last) {
+      pop_back();
+    }
+    // case 3: remove from middle
+    else {
+      Node* temp = i.node_ptr;
+      i.node_ptr->prev->next = i.node_ptr->next;
+      i.node_ptr->next->prev = i.node_ptr->prev;
+      delete(temp);
+    }
+
+  }
 
   //REQUIRES: i is a valid iterator associated with this list
   //EFFECTS: inserts datum before the element at the specified position.
-  void insert(Iterator i, const T &datum);
+  void insert(Iterator i, const T &datum) {
+    if(i.node_ptr == first) {
+      push_front(datum);
+    }
+    else {
+      Node* n = new Node();
+      n->datum = datum;
+      n->prev = i.node_ptr->prev;
+      n->next = i.node_ptr;
+      i.node_ptr->prev->next = n;
+      i.node_ptr->prev = n;
+    }
+  }
 
 };//List
 
--- List_tests.cpp
+++ List_tests.cpp
@@ -5,9 +5,218 @@ using namespace std;
 
 // Add your test cases here
 
-TEST(test_stub) {
-    // Add test code here
-    ASSERT_TRUE(true);
+TEST(test_list_default_ctor) {
+    List<int> empty_list;
+    ASSERT_TRUE(empty_list.empty());
+}
+
+// TODO add tests for asserts()
+
+
+TEST(test_add_front_empty) {
+    List<int> list;
+    list.push_front(4);
+    ASSERT_FALSE(list.empty());
+    ASSERT_TRUE(list.size() == 1);
+
+}
+
+TEST(test_add_front_twice) {
+    List<int> list;
+    list.push_front(4);
+    list.push_front(5);
+    ASSERT_TRUE(list.size() == 2);
+    ASSERT_TRUE(list.front() == 5);
+    ASSERT_TRUE(list.back() == 4);
+}
+
+TEST(test_push_back) {
+    List<int> list;
+    list.push_back(4);
+    list.push_back(5);
+    ASSERT_TRUE(list.size() == 2);
+    ASSERT_TRUE(list.front() == 4);
+    ASSERT_TRUE(list.back() == 5);
+}
+
+TEST(pop_front_one_item) {
+    List<int> list;
+    list.push_front(4);
+    list.pop_front();
+    ASSERT_TRUE(list.size() == 0);
+    ASSERT_TRUE(list.empty());
+}
+
+TEST(pop_front_two_items) {
+    List<int> list;
+    list.push_front(4);
+    list.push_back(5);
+    ASSERT_TRUE(list.size() == 2);
+    list.pop_front();
+    ASSERT_TRUE(list.size() == 1);
+    ASSERT_TRUE(list.front() == 5);
+    ASSERT_TRUE(list.back() == 5);
+    list.pop_front();
+    ASSERT_TRUE(list.size() == 0);
+}
+
+TEST(pop_back_one_item) {
+    List<int> list;
+    list.push_front(4);
+    list.pop_back();
+    ASSERT_TRUE(list.size() == 0);
+    ASSERT_TRUE(list.empty());
+}
+
+TEST(pop_back_two_items) {
+    List<int> list;
+    list.push_front(4);
+    list.push_back(5);
+    ASSERT_TRUE(list.size() == 2);
+    list.pop_back();
+    ASSERT_TRUE(list.size() == 1);
+    ASSERT_TRUE(list.front() == 4);
+    ASSERT_TRUE(list.back() == 4);
+    list.pop_back();
+    ASSERT_TRUE(list.size() == 0);
+}
+
+TEST(clear) {
+    List<int> list;
+    list.push_front(3);
+    list.push_front(4);
+    list.push_back(5);
+    list.pop_front();
+    list.clear();
+    ASSERT_TRUE(list.size() == 0);
+    list.push_front(7);
+    ASSERT_TRUE(list.size() == 1);
+    ASSERT_TRUE(list.front() == 7);
+    ASSERT_TRUE(list.back() == 7);
+}
+
+TEST(copy_ctor) {
+    List<int> list1;
+    
+    list1.push_front(1);
+    list1.push_back(2);
+    list1.push_back(3);
+
+    List<int> list2(list1);
+
+    ASSERT_TRUE(list1.size() == 3);
+    ASSERT_TRUE(list2.size() == 3);
+    ASSERT_TRUE(list1.front() == 1);
+    ASSERT_TRUE(list2.front() == 1);
+    list1.pop_front();
+    ASSERT_TRUE(list1.front() == 2);
+    ASSERT_TRUE(list2.front() == 1);
+}
+
+TEST(iterator_plusplus_star_begin_end_eq_noteq) {
+    List<int> list;
+    list.push_back(3);
+    list.push_back(4);
+    list.push_back(5);
+    list.push_back(6);
+    List<int>::Iterator itr = list.begin();
+    int expected[] = {3, 4, 5, 6};
+    int i = 0;
+
+    for(; itr != list.end(); ++itr) {
+        ASSERT_TRUE(*itr == expected[i]);
+        i++;
+    }
+}
+
+TEST(iterator_minusminus) {
+    List<int> list;
+    list.push_back(7);
+    list.push_back(8);
+    list.push_back(9);
+    List<int>::Iterator itr = list.begin();
+    ++itr;
+    ASSERT_TRUE(*itr == 8);
+    --itr;
+    ASSERT_TRUE(*itr == 7);
+}
+
+TEST(iterator_erase_front) {
+    List<int> list;
+    list.push_back(7);
+    list.push_back(8);
+    list.push_back(9);
+    List<int>::Iterator itr = list.begin();
+    ASSERT_TRUE(list.front() == 7);
+    ASSERT_TRUE(list.size() == 3);
+    list.erase(itr);
+    ASSERT_TRUE(list.front() == 8);
+    ASSERT_TRUE(list.size() == 2);
+}
+
+TEST(iterator_erase_middle) {
+    List<int> list;
+    list.push_back(7);
+    list.push_back(8);
+    list.push_back(9);
+    List<int>::Iterator itr = list.begin();
+    ++itr;
+    ASSERT_TRUE(list.front() == 7);
+    ASSERT_TRUE(list.size() == 3);
+    list.erase(itr);
+    ASSERT_TRUE(list.front() == 7);
+    ASSERT_TRUE(list.size() == 2);
+    list.pop_front();
+    ASSERT_TRUE(list.front() == 9);
+    ASSERT_TRUE(list.size() == 1);
+}
+
+TEST(iterator_erase_end) {
+    List<int> list;
+    list.push_back(7);
+    list.push_back(8);
+    list.push_back(9);
+    List<int>::Iterator itr = list.begin();
+    ++itr;
+    ++itr;
+    ASSERT_TRUE(list.front() == 7);
+    ASSERT_TRUE(list.size() == 3);
+    list.erase(itr);
+    ASSERT_TRUE(list.front() == 7);
+    ASSERT_TRUE(list.size() == 2);
+    list.pop_back();
+    ASSERT_TRUE(list.front() == 7);
+    ASSERT_TRUE(list.size() == 1);
+}
+
+TEST(iterator_insert_front) {
+    List<int> list;
+    list.push_back(7);
+    list.push_back(8);
+    list.push_back(9);
+    List<int>::Iterator itr = list.begin();
+    ASSERT_TRUE(list.front() == 7);
+    ASSERT_TRUE(list.size() == 3);
+    list.insert(itr, 6);
+    ASSERT_TRUE(list.front() == 6);
+    ASSERT_TRUE(list.size() == 4);
+}
+
+TEST(iterator_insert_not_front) {
+    List<int> list;
+    list.push_back(7);
+    list.push_back(9);
+    list.push_back(10);
+    List<int>::Iterator itr = list.begin();
+    ++itr;
+    ASSERT_TRUE(list.front() == 7);
+    ASSERT_TRUE(list.size() == 3);
+    list.insert(itr, 8);
+    ASSERT_TRUE(list.front() == 7);
+    ASSERT_TRUE(list.size() == 4);
+    list.pop_front();
+    ASSERT_TRUE(list.front() == 8);
+    ASSERT_TRUE(list.size() == 3);
 }
 
 TEST_MAIN()
